b95d0ba561fb8314dcb24ba997e0d5f0266a0d93
ahadkhantb <mdabdulahadtb468@gmail.com>
2025-02-10 15:13:56 +0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyAPY3UMjxg-Zdbb0M-049qY3lqQd8X6DIM",
     authDomain: "https://follow-shop-bd-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "follow-shop-bd",
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
